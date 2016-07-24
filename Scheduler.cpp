bab0b3e063c6d1d1cf309dba003d0deb10e9b357
kavya <kavya.bdj@gmail.com>
2016-07-24 10:40:57 +0530

    Scheduler has been re-structured

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -17,8 +17,8 @@ typedef struct
 
 typedef struct
 {
-	pthread_mutex_t* slock;
-	std::queue<Task*> *sTaskList;
+	pthread_mutex_t* lock;
+	std::queue<Task*> *TaskList;
 }ThreadData;
 
 void CreateTasks(int totalNoOfTasks, std::queue<Task*> *taskList)
@@ -34,50 +34,56 @@ void CreateTasks(int totalNoOfTasks, std::queue<Task*> *taskList)
 	}
 }
 
-void* ExecuteTask(void* p)
+Task* GetTask(ThreadData* threadData)
+{
+	std::queue<Task*> *taskList = threadData->TaskList;
+	pthread_mutex_t *lock = threadData->lock;
+	Task* newTask=NULL;
+	pthread_mutex_lock(lock);
+	if (!taskList->empty())
+	{
+		newTask = taskList->front();
+		taskList->pop();
+	}
+	pthread_mutex_unlock(lock);
+	return newTask;
+}
+
+void ExecuteTask(Task* task)
+{
+	switch (task->operation)
+	{
+		case kAdd: cout << task->op1 + task->op2 << endl;
+			   break;
+		case kSub: cout << task->op1 - task->op2 << endl;
+			   break;
+		default:
+			   cout << "Sorry Chethan!" << endl;
+			   break;
+	}
+
+}
+
+void* AssignTask(void* p)
 {
 	ThreadData *threadData = static_cast<ThreadData*>(p);
-	pthread_mutex_t *lock = threadData->slock;
-	std::queue<Task*> *taskList = threadData->sTaskList;
-	Task *newTask = NULL;
-	cout << "entering ExecuteTask" << endl;
-	cout << "lock obj address" << lock << endl;
-	cout << "total tasks " << taskList->size();
-	while (true)
+	pthread_mutex_t *lock = threadData->lock;
+	std::queue<Task*> *taskList = threadData->TaskList;
+	Task *task = NULL;
+	while ((task = GetTask(threadData)) != NULL)
 	{
-		pthread_mutex_lock(lock);
-		if (!taskList->empty())
-		{
-			newTask = taskList->front();
-			taskList->pop();
-		}
-		else
-		{
-			pthread_mutex_unlock(lock);
-			break;
-		}
-		pthread_mutex_unlock(lock);
-		switch (newTask->operation)
-		{
-			case kAdd: cout << newTask->op1 + newTask->op2 << endl;
-				   break;
-			case kSub: cout << newTask->op1 - newTask->op2 << endl;
-				   break;
-			default:
-				   cout << "Sorry Chethan!" << endl;
-				   break;
-		}
+		ExecuteTask(task);
 	}
 }
 
 int main(int argc, char *argv[])
 {
 	std::queue<Task*> *TaskList = new std::queue<Task*>;
-	
+
 	// 1. Create Tasks
 	// 2. Pool/Queue the tasks
 	CreateTasks(100,TaskList);
-	
+
 	// 3. Create Worker Thread Pool
 	const int numThreads = 4;
 	pthread_t ThreadId[numThreads];
@@ -86,14 +92,14 @@ int main(int argc, char *argv[])
 	ThreadData threadData;
 	threadData.lock = &lock;
 	threadData.TaskList = TaskList;
-	
+
 	// 4. Execute the tasks
 	for (int i = 0; i < numThreads ; ++i)
 	{
 		cout << "creating thread "<< i << endl;
-		pthread_create(&ThreadId[i], NULL, ExecuteTask, &threadData);
+		pthread_create(&ThreadId[i], NULL, AssignTask, &threadData);
 	}
-	
+
 	for (int i = 0 ; i < numThreads ; ++i)
 	{
 		pthread_join(ThreadId[i], NULL);
